daec5ae33147d2e2736069567e5ea233d0f1d4e1
zCitrus <johnuelph@gmail.com>
2026-08-16 13:18:09 +0800

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,14 @@
+-- Configuration settings for the Reanimation system
+local Config = {
+    HatCollide = false,
+    HideCharacter = false,
+    CustomHat = false,
+    RigTransparency = 1,
+    R15 = false,
+    BreakJointsDelay = 0.1,
+    SetCameraSubject = true,
+    DisableCharacterCollisions = true,
+    SimulationRadius = 2147483647,
+}
+
+return Config
